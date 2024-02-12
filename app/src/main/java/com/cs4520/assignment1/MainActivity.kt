5749b06ff5ece96e2eb4355b335d1adf6178b97f
coreyan1234 <an.co@northeastern.edu>
2024-02-12 01:45:56 -0500

    successfully created the LoginFragment

--- app/src/main/java/com/cs4520/assignment1/MainActivity.kt
+++ app/src/main/java/com/cs4520/assignment1/MainActivity.kt
@@ -2,10 +2,17 @@ package com.cs4520.assignment1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cs4520.assignment1.fragments.LoginFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, LoginFragment())
+                .commit()
+        }
     }
 }
--- app/src/main/java/com/cs4520/assignment1/fragments/LoginFragment.kt
+++ app/src/main/java/com/cs4520/assignment1/fragments/LoginFragment.kt
@@ -1,4 +1,43 @@
 package com.cs4520.assignment1.fragments
 
-class LoginFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.cs4520.assignment1.R
+
+class LoginFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        val usernameField = view.findViewById<EditText>(R.id.usernameField)
+        val passwordField = view.findViewById<EditText>(R.id.passwordField)
+        val loginButton = view.findViewById<Button>(R.id.buttonLogin)
+
+        loginButton.setOnClickListener {
+            val username = usernameField.text.toString()
+            val password = passwordField.text.toString()
+
+            if (username == "admin" && password == "admin") {
+                findNavController().navigate(R.id.action_loginFragment_to_productListFragment)
+            }
+            else {
+                Toast.makeText(requireContext(), "Invalid username or password", Toast.LENGTH_SHORT).show()
+            }
+
+            usernameField.text.clear()
+            passwordField.text.clear()
+        }
+
+        return view
+    }
 }
